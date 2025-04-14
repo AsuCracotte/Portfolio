ed04f638d7dda16a923def04f85fa9e63ae86454
Asuna <eva.lariccia@lamache.org>
2025-04-14 12:17:29 +0200

    Update button animation on 404 page: adjust vertical position and refine TODO for hover effect

--- js/404.js
+++ js/404.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     gsap.from(".button", {
-        y: 100,
+        y: 50,
         opacity: 0,
         duration: 1.5,
         ease: "power2.out",
+        clearProps: "all",
     });
 });
